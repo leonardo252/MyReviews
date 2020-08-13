432c26f773b51252f3f8d0df446385985b8426dd
Leonardo Gomes Fernandes <leonardo-lol252@hotmail.com>
2020-08-13 12:01:20 -0300

    Initial Commit

---
+++ MyReviews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyReviews
+//
+//  Created by Leonardo Gomes on 13/08/20.
+//  Copyright © 2020 Leonardo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
