69ae81efcfed3ce1b911885693dba9a0814ea9db
Leonardo Fernandes <leonardo.gomes@lit.ifce.edu.br>
2020-08-14 12:28:34 -0300

    Added SwiftLint to project

--- MyReviews/SceneDelegate.swift
+++ MyReviews/SceneDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -48,6 +49,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
